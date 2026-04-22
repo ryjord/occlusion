fbdd7622008756995ef7feab3cdcc217067c3cdc
Riley <rileyjordan21@hotmail.com>
2026-04-22 22:18:48 +0100

    Test

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Default
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
